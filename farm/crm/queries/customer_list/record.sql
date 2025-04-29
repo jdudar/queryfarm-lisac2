fcb2cd460eb805e3f700895a0217b6a5f2a7b508
jdudar <jdudar@lisalumber.com>
2025-04-29 12:20:28 -0700

    Update record.sql

--- farm/crm/queries/customer_list/record.sql
+++ farm/crm/queries/customer_list/record.sql
@@ -1,3 +1,28 @@
+CREATE TABLE customers(
+    cust_id VARCHAR,
+    cust_name VARCHAR,
+    email VARCHAR,
+    phone VARCHAR
+)
+VALUES
+    ('C100','Customer 100','john@gmail.com','800-232-4423')
+    ('C101','Customer 101','mike@gmail.com','552-202-0923')
+    ('C101','Customer 101','susan@gmail.com','602-442-0909')
+
+
+CREATE FRAME main
+AS RETURN PUT.LIST(
+    @data = (SELECT * FROM customer c)
+    @title = 'Customer list'
+    @columns = (
+        PUT.COLUMN(c.cust_id)
+        PUT.COLUMN(c.cust_name)
+        PUT.COLUMN(c.email)
+        PUT.COLUMN(c.phone)
+    )
+    @onrowclick = OPEN(record,@cust_id = c.cust_id)
+)
+
 CREATE FRAME record
     @cust_id LIKE customer.cust_id
 AS BEGIN
